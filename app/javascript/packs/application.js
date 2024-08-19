1ecb2f7a84ca464223b8c128737638f8a38f4f20
kmSidharthM <kmsidharthmurali@gmail.com>
2024-08-19 22:04:18 +0530

    Setup Tailwind CSS

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,3 +1,4 @@
+import "../stylesheets/application.scss"
 /* eslint no-console:0 */
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
