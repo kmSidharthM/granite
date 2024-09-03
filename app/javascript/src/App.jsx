98029a816338cfb818b1bf683b72c7d11f95b547
kmSidharthM <kmsidharthmurali@gmail.com>
2024-09-03 12:36:19 +0530

    Created Toastr component and added Axios interceptors

--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -1,16 +1,15 @@
 import React from "react";
 
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 
+import Dashboard from "components/Dashboard";
 import CreateTask from "components/Tasks/Create";
-
-import Dashboard from "./components/Dashboard";
 
 const App = () => (
   <Router>
+    <ToastContainer />
     <Switch>
-      <Route exact path="/" render={() => <div>Home</div>} />
-      <Route exact path="/about" render={() => <div>About</div>} />
       <Route exact component={CreateTask} path="/tasks/create" />
       <Route exact component={Dashboard} path="/dashboard" />
     </Switch>
